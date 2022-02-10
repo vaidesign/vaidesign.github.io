5074de29bcf762ad5b62d8384ca36e0ff1fb07d4
Ajit Rajurkar <post.vaishalirajurkar@gmail.com>
2022-02-09 23:27:07 -0800

    about

--- js/about.js
+++ js/about.js
@@ -72,3 +72,12 @@ $(window).scroll(function(){
   }
  });
 });
+
+
+
+$(window).load(function() {
+	$('#loader').addClass('is_active');
+	setTimeout(function() {
+		$('#loader').addClass('is_loaded');
+	}, 15000);
+});
